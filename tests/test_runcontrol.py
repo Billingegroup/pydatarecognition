068a03af2ac5c1f58f886e9b339cfbfe9f69a38d
Robin Lee <sl5035@columbia.edu>
2023-03-21 20:01:56 -0400

    runcontrol test bug fix

--- tests/test_runcontrol.py
+++ tests/test_runcontrol.py
@@ -4,20 +4,15 @@ import json
 from testfixtures import TempDirectory
 from pathlib import Path
 
-from pydatarecognition.runcontrol import DEFAULT_RC, filter_databases, \
-    connect_db
+from pydatarecognition.runcontrol import filter_databases, connect_db
 from pydatarecognition.database import connect
-from tests.inputs.pydr_rc import pydr_rc
 
 
-def test_connect_db():
-    rc = copy.copy(DEFAULT_RC)
-    rc._update(pydr_rc)
-
+def test_connect_db(rc):
     filter_databases(rc)
     with connect(rc) as rc.client:
         expected_dbs = rc.client.dbs
         expected_chdb = rc.client.chained_db
-    chained_db, dbs = connect_db(rc)
-    assert chained_db == expected_chdb
+    dbs = connect_db(rc)
+    # assert chained_db == expected_chdb
     assert dbs == expected_dbs
